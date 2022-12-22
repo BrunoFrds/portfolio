4d3937f9e0422f10be31444fd0f86c0471ec0764
BrunoFrds <bruno_fernandes@live.fr>
2022-12-22 17:14:12 +0100

    Création du composant erreur

--- src/Pages/Erreur/Erreur.jsx
+++ src/Pages/Erreur/Erreur.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Erreur = () => {
-  return <div>err</div>;
+  return (
+    <div className="error-page">
+      <h1 className="error">404</h1>
+      <p className="alert">Oups! La page que vous demandez n'éxiste pas.</p>
+      <NavLink to="/" className="redirection">
+        Retour vers l'accueil
+      </NavLink>
+    </div>
+  );
 };
 
 export default Erreur;
