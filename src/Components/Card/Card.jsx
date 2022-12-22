5237569cd7fbd599ca4ff540a368a9b9e01a8a88
BrunoFrds <bruno_fernandes@live.fr>
2022-12-22 17:11:33 +0100

    Création des composants card et portfolio

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-const Card = () => {
-  return <div>card</div>;
+const Card = ({ name, image, description, icon, lien }) => {
+  return (
+    <div className="card">
+      <img src={image} alt="Projet" className="card__img" />
+      <div className="card__info">
+        <h1 className="card__name">{name}</h1>
+        <div className="card__icon">{icon}</div>
+        <p className="card__description">{description}</p>
+        <a className="card__lien" href={lien}>
+          <FontAwesomeIcon icon={faGithub} className="lien-button" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import ListeProjet from "../../Assets/data/Projets";
+import Card from "../../Components/Card/Card";
 
 const Portfolio = () => {
-  return <div>port</div>;
+  const listeprojet = ListeProjet.map((proj) => proj.picture);
+  console.log(listeprojet);
+  return (
+    <div className="portfolio-page">
+      <div className="title-page">
+        <h1 className="title-page__portfolio">Projets</h1>
+      </div>
+      <div className="portfolio-list">
+        <div className="portfolio-list__projet">
+          {ListeProjet.map((projet) => (
+            <Card
+              key={projet.id}
+              id={projet.id}
+              name={projet.name}
+              image={projet.picture}
+              description={projet.description}
+              icon={projet.languagesIcons}
+              lien={projet.github}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
