18bb55ec07694d2d8f30edb18af45d1467e78b86
BrunoFrds <bruno_fernandes@live.fr>
2022-12-22 17:10:21 +0100

    Création des composants progressbar et competences

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
-const ProgressBar = () => {
-  return <div>prog</div>;
+const ProgressBar = ({ title, value }) => {
+  return (
+    <div className="container">
+      <div className="progressbar">
+        <span className="progressbar__title">{title}</span>
+        <div className="progressbar__skill">
+          <span className={`progressbar__skill-per ${title}`}></span>
+          <span className="tooltip">{value}%</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProgressBar;
--- src/Pages/Competences/Competences.jsx
+++ src/Pages/Competences/Competences.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import ProgressBar from "../../Components/ProgressBar/ProgressBar";
 
 const Competences = () => {
-  return <div>commp</div>;
+  return (
+    <div className="skills-page">
+      <div className="title-page">
+        <h1 className="title-page__skills">Compétences</h1>
+      </div>
+      <div className="competences">
+        <div className="skills">
+          <h1 className="skills-title">Front-End Skills</h1>
+          <ProgressBar title="HTML" value="100" />
+          <ProgressBar title="CSS" value="95" />
+          <ProgressBar title="JavaScript" value="75" />
+          <ProgressBar title="ReactJS" value="65" />
+        </div>
+        <div className="skills">
+          <h1 className="skills-title">Back-End Skills</h1>
+          <ProgressBar title="NodeJS" value="30" />
+          <ProgressBar title="ExpressJS" value="30" />
+          <ProgressBar title="MongoDB" value="30" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Competences;
