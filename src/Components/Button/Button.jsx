abd70f46c3597a9d8e2c22f772afc41df1e6a37e
BrunoFrds <brunofrdsdev@gmail.com>
2023-09-26 12:32:45 +0200

    correction

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,6 +1,11 @@
 const Button = ({ lienContact, lienName, lienIcon }) => {
   return (
-    <a href={lienContact} target="_blank" className="lien-contact">
+    <a
+      href={lienContact}
+      target="_blank"
+      rel="noreferrer"
+      className="lien-contact"
+    >
       <h1 className="title-contact">{lienName}</h1>
       <p className="icon-contact">{lienIcon}</p>
     </a>
